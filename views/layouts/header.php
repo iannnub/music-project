e2f69da729d09d38bd23e664f4031a3ab9c2a772
iannnub <150099114+iannnub@users.noreply.github.com>
2026-01-26 02:41:17 +0700

    seo

--- views/layouts/header.php
+++ views/layouts/header.php
@@ -2,33 +2,35 @@
 <html lang="id">
 
 <head>
-    <meta charset="utf-8">
+    <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-    
-    <title>KakYo Lesson | Kursus Musik Private & Reguler Terbaik</title>
-    <meta name="description" content="Olah hobimu menjadi prestasi bersama KakYo Lesson. Kursus musik Vocal, Gitar, Bass, Piano, dan Band dengan jadwal fleksibel di Jember.">
-    <meta name="keywords" content="les musik jember, kursus musik private, kakyo lesson, belajar gitar jember, les vokal terbaik">
-    <meta name="author" content="Septian Putra">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no">
 
-    <meta property="og:type" content="website">
-    <meta property="og:url" content="https://kakyolesson.infy.uk/">
+    <title>KakYo Lesson | Kursus Musik Terbaik by Septian Putra</title>
+    <meta name="description" content="Olah hobimu menjadi prestasi bersama KakYo Lesson. Sistem manajemen kursus musik modern di Jember.">
+    <meta name="theme-color" content="#4e73df"> <meta property="og:type" content="website">
+    <meta property="og:url" content="https://kakyolesson.page.gd/">
+    <meta property="og:site_name" content="KakYo Lesson">
     <meta property="og:title" content="KakYo Lesson - Olah Hobimu Menjadi Prestasi">
-    <meta property="og:description" content="Gabung sekarang di KakYo Lesson! Kursus musik profesional dengan kurikulum modern dan jadwal yang fleksibel.">
-    <meta property="og:image" content="https://kakyolesson.infy.uk/assets/img/bg-login.png"> <meta property="twitter:card" content="summary_large_image">
-    <meta property="twitter:url" content="https://kakyolesson.infy.uk/">
-    <meta property="twitter:title" content="KakYo Lesson | Kursus Musik Terbaik">
-    <meta property="twitter:description" content="Kursus vokal, gitar, bass, piano, dan band di Jember. Cek perkembangan siswamu secara transparan di sini.">
-    <meta property="twitter:image" content="https://kakyolesson.infy.uk/assets/img/bg-login.png">
+    <meta property="og:description" content="Manajemen kursus musik Vocal, Gitar, Bass, Piano, dan Band terbaik by Septian Putra.">
+    
+    <meta property="og:image" content="https://kakyolesson.page.gd/assets/img/logo-fix.png">
+    <meta property="og:image:width" content="600">
+    <meta property="og:image:height" content="600">
 
-    <link rel="icon" href="assets/img/logo-fix.png" type="image/png">
-    <link rel="apple-touch-icon" href="assets/img/logo-fix.png"> <link href="assets/sb-admin-2/vendor/fontawesome-free/css/all.min.css" rel="stylesheet" type="text/css">
+    <meta name="twitter:card" content="summary_large_image">
+    <meta name="twitter:title" content="KakYo Lesson - Premium Music Management">
+    <meta name="twitter:description" content="Sistem Informasi Manajemen Kursus Musik Profesional.">
+    <meta name="twitter:image" content="https://kakyolesson.page.gd/assets/img/logo-fix.png">
+
+    <link rel="icon" type="image/png" href="assets/img/logo-fix.png">
+    <link rel="apple-touch-icon" href="assets/img/logo-fix.png">
+
+    <link href="assets/sb-admin-2/vendor/fontawesome-free/css/all.min.css" rel="stylesheet" type="text/css">
     <link href="https://fonts.googleapis.com/css?family=Nunito:200,200i,300,300i,400,400i,600,600i,700,700i,800,800i,900,900i" rel="stylesheet">
-
     <link href="assets/sb-admin-2/css/sb-admin-2.min.css" rel="stylesheet">
+    
     <script src="assets/sb-admin-2/vendor/jquery/jquery.min.js"></script>
-
-    <link rel="canonical" href="https://kakyolesson.infy.uk/" />
 </head>
 
 <body id="page-top">
